46480c61b4d91226b2ecd1e7f0f94d0acffecdec
liangtang <liang.tang@celohealth.com>
2020-10-20 14:25:09 +1300

    set default state for ProductDetails

--- app/src/main/java/com/tlnacl/reactiveapp/ui/detail/ProductDetailsViewModel.kt
+++ app/src/main/java/com/tlnacl/reactiveapp/ui/detail/ProductDetailsViewModel.kt
@@ -1,23 +1,19 @@
 package com.tlnacl.reactiveapp.ui.detail
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.tlnacl.reactiveapp.businesslogic.http.ProductBackendApiDecorator
 import com.tlnacl.reactiveapp.businesslogic.model.ProductDetail
 import com.tlnacl.reactiveapp.uniflow.DataFlowBaseViewModel
 import com.tlnacl.reactiveapp.uniflow.data.UIState
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class ProductDetailsViewModel @Inject constructor(private val api: ProductBackendApiDecorator): DataFlowBaseViewModel() {
+class ProductDetailsViewModel @Inject constructor(private val api: ProductBackendApiDecorator)
+    : DataFlowBaseViewModel(defaultState = UIState.Loading) {
 
     fun getDetail(productId: Int) = action(
             onAction = {
                 val product = api.getProduct(productId)
                 setState(ProductDetailsViewState(ProductDetail(product, false)))
             },
-            onError = { error,_ -> setState { UIState.Failed("getProduct failed", error) }}
+            onError = { error, _ -> setState { UIState.Failed("getProduct failed", error) } }
     )
 }
